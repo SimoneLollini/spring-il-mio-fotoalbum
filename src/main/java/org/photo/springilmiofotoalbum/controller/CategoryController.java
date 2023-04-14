44f3b72815e7f6bb20f3fc9d9bb9d471ceb892fd
SimoneLollini <lollinisimone@gmail.com>
2023-04-14 12:30:48 +0200

    Add categories crud

---
+++ src/main/java/org/photo/springilmiofotoalbum/controller/CategoryController.java
@@ -0,0 +1,100 @@
+package org.photo.springilmiofotoalbum.controller;
+
+import jakarta.validation.Valid;
+import org.photo.springilmiofotoalbum.model.Category;
+import org.photo.springilmiofotoalbum.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Category> categories;
+        categories = categoryService.getAllCategory();
+        model.addAttribute("categories", categories);
+        return "/categories/index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        Category newCategory = new Category();
+        model.addAttribute("category", newCategory);
+        return "/categories/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("category") Category formCategory, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors())
+            return "/categories/create";
+        categoryService.createCategory(formCategory);
+        redirectAttributes.addFlashAttribute("message", "Categoria aggiunta!");
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model) {
+        try {
+            Category category = categoryService.getById(id);
+            model.addAttribute("category", category);
+            List<Category> categories = categoryService.getAllCategory();
+            model.addAttribute("categories", categories);
+            return "/categories/edit";
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria non trovata");
+        }
+    }
+
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable("id") Integer id, @Valid @ModelAttribute("category") Category formCategory, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors())
+            return "/categories/edit";
+        try {
+            Category updatedCategory = categoryService.updateCategory(formCategory, id);
+            redirectAttributes.addFlashAttribute("message", "Categoria modificata!");
+            return "redirect:/categories";
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria non trovata!");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        try {
+            Category category = categoryService.getById(id);
+            model.addAttribute("category", category);
+            return "/categories/show";
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria non trovata");
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        try {
+            boolean success = categoryService.deleteById(id);
+            if (success)
+                redirectAttributes.addFlashAttribute("message", "Categoria eliminata!");
+            else {
+                redirectAttributes.addFlashAttribute("message", "Non puoi eliminare!");
+            }
+        } catch (RuntimeException e) {
+            redirectAttributes.addFlashAttribute("message", "Pizza non trovata!");
+        }
+        return "redirect:/categories";
+    }
+
+}
--- src/main/java/org/photo/springilmiofotoalbum/service/CategoryService.java
+++ src/main/java/org/photo/springilmiofotoalbum/service/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -15,5 +16,36 @@ public class CategoryService {
 
     public List<Category> getAllCategory() {
         return categoryRepository.findAll(Sort.by("name"));
+    }
+
+    public Category getById(Integer id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        if (category.isPresent())
+            return category.get();
+        else
+            throw new RuntimeException(Integer.toString(id));
+    }
+
+    public Category createCategory(Category formCategory) throws RuntimeException {
+        Category newCategory = new Category();
+        newCategory.setId(formCategory.getId());
+        newCategory.setName(formCategory.getName());
+        return categoryRepository.save(newCategory);
+    }
+
+    public Category updateCategory(Category formCategory, Integer id) throws RuntimeException {
+        Category categoryToUpdate = getById(id);
+        categoryToUpdate.setId(formCategory.getId());
+        categoryToUpdate.setName(formCategory.getName());
+        return categoryRepository.save(categoryToUpdate);
+    }
+
+    public boolean deleteById(Integer id) throws RuntimeException {
+        try {
+            categoryRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
