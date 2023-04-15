d361e31f9f06d61d7b234e73ce96918dfab9c17d
SimoneLollini <lollinisimone@gmail.com>
2023-04-15 20:42:17 +0200

    add front-end tested callApi

---
+++ front-end/src/components/AppHeader.vue
@@ -0,0 +1,51 @@
+<script>
+import { store } from "../store.js";
+import SearchBar from './SearchBar.vue';
+export default {
+    name: 'AppHeader',
+    components: {
+        SearchBar,
+    }, data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="container d-flex justify-content-between align-items-center">
+            <SearchBar />
+        </div>
+    </header>
+    <div class="hero_image"></div>
+</template>
+
+<style lang="scss" scoped>
+header {
+    height: 100vh;
+    z-index: 1000;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.808), rgba(19, 19, 19, 0.109));
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+
+    .container {
+        margin-top: 2rem;
+
+        img.logo {
+            width: 180px;
+        }
+    }
+}
+
+.hero_image {
+    height: 98vh;
+    background: black;
+    background-image: url(../assets/img/bg-home.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
---
+++ front-end/src/components/SearchBar.vue
@@ -0,0 +1,57 @@
+<script>
+import { store } from "../store.js";
+export default {
+    name: 'SearchBar',
+    emits: ['CallSearch'],
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div class="search_bar gap-2 pt-2">
+        <input class="form_control" @keyup.enter="$emit('CallSearch')" type="text" placeholder="Cerca"
+            v-model="store.photoToSearch">
+        <button class="btn_danger" @click="store.callApi(store.API_URL_PHOTO)">Cerca</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables.scss' as *;
+
+.search_bar {
+    display: flex;
+    justify-content: center;
+
+    input {
+        font-weight: 200;
+        color: white;
+        border-radius: 5px;
+        height: 1.5rem;
+        width: 180px;
+        background-color: #333333;
+        border: none;
+        border-bottom: 1px solid #8c8c8c;
+        padding: 0 10px;
+    }
+
+    ::placeholder {
+        opacity: 0.3;
+        font-weight: 200;
+        color: white;
+    }
+
+    .btn_danger {
+        background-color: $bf_danger;
+        border: none;
+        border-radius: 5px;
+        color: $light;
+        font-size: 0.9rem;
+        font-weight: 300;
+    }
+}
+</style>
